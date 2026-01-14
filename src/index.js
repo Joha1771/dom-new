48faaf969bbf981293755a6aafbc36dc433ab901
Joha1771 <johasalimov1717@gmail.com>
2026-01-14 00:38:46 -0500

    completed fully

--- src/index.js
+++ src/index.js
@@ -6,65 +6,85 @@ let carPos = 0;
 let isId = null;
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "r" && !isId) {
-        body.style.backgroundColor = ""; 
-        
-        isId = setInterval(() => {
-            carPos += 10;
-            runCar.style.transform = `translateX(${carPos}px)`;
+  if (e.key === "r" && !isId) {
+    body.style.backgroundColor = "";
 
-            if (carPos >= 1100) {
-                clearInterval(isId); 
-                isId = null;
-                sectionBg.style.backgroundColor = "green"; 
-            }
-        }, 50);
-    }
-    if (e.key === "s") {
+    isId = setInterval(() => {
+      carPos += 10;
+      runCar.style.transform = `translateX(${carPos}px)`;
+
+      if (carPos >= 1100) {
         clearInterval(isId);
         isId = null;
-        
+        sectionBg.style.backgroundColor = "green";
+      }
+    }, 50);
+  }
+  if (e.key === "s") {
+    clearInterval(isId);
+    isId = null;
 
-        carPos = 0;
-        runCar.style.transform = `translateX(0px)`;
-        sectionBg.style.backgroundColor = "";
-    }
+    carPos = 0;
+    runCar.style.transform = `translateX(0px)`;
+    sectionBg.style.backgroundColor = "";
+  }
 });
 
 // 2
 
-
 const topElement = document.querySelector(".top-car");
 const topBody = document.body;
 
-let posX = 0; 
-let posY = 0; 
+let posX = 0;
+let posY = 0;
 let driveId = null;
 
 document.addEventListener("keydown", (e) => {
+  if (e.key === "b" && !driveId) {
+    driveId = setInterval(() => {
+      posY -= 5;
+      if (posY < -600) {
+        posY = 90;
+      }
+      topElement.style.transform = `translate(${posX}px, ${posY}px)`;
+    }, 30);
+  }
 
-    if (e.key === "b" && !driveId) {
-        driveId = setInterval(() => {
-            posY -= 5; 
-            if (posY < -600) {
-                posY = 90;
+  if (e.key === "ArrowRight") {
+    posX += 300;
+    if (posX > 300) {
+      posX = 300;
+    }
+  }
+  if (e.key === "ArrowLeft") {
+    posX -= 300;
+    if (posX < -300) {
+      posX = -300;
+    }
+  }
+  topElement.style.transform = `translate(${posX}px, ${posY}px)`;
+});
 
-            }
-            topElement.style.transform = `translate(${posX}px, ${posY}px)`;
-        }, 30);
-    }
+// 3
 
-    if (e.key === "ArrowRight") {
-        posX += 300;
-        if(posX > 300){
-            posX = 300
-        } 
+const roundElement = document.querySelector(".redRound");
+let scalePoint = 1.0;
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "+") {
+    scalePoint += 0.1;
+    roundElement.style.transform = `scale(${scalePoint})`;
+  } else if (e.key === "-") {
+    if (scalePoint <= 0.2) {
+      alert("No go! Too small.");
+    } else {
+      scalePoint -= 0.1;
+      roundElement.style.transform = `scale(${scalePoint})`;
     }
-    if (e.key === "ArrowLeft") {
-        posX -= 300; 
-        if(posX < -300){
-            posX = -300
-        } 
+  } else if (e.key === "t") {
+    roundElement.style.backgroundColor = "yellow";
+  }
+  else if (e.key === "y") { 
+        roundElement.style.backgroundColor = ""; 
     }
-    topElement.style.transform = `translate(${posX}px, ${posY}px)`;
 });
